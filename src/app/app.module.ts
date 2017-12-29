3f9d6bd628b835c5c541465c740c0ff03af7b31a
Ibrahim Wickama <ibrahimwickama@gmail.com>
2017-12-29 21:36:44 +0300

    add charts card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AdminModeComponent } from './pages/admin-mode/admin-mode.component';
 import { UserModeComponent } from './pages/user-mode/user-mode.component';
+import {ChartsComponent} from "./analysis-components/charts/charts.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminModeComponent,
-    UserModeComponent
+    UserModeComponent,
+    ChartsComponent
   ],
   imports: [
     BrowserModule
